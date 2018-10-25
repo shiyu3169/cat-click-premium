3827774d0d63f5ae4482fa1722a050c320d3808e
Shiyu Wang <shiyu3169@gmail.com>
2018-10-25 13:42:55 -0400

    separation of concern

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ var cats = [
 
 //view
 const catList = document.querySelector("#cats");
+const display = document.querySelector("#display");
 
 function initList(cats) {
     for (cat of cats) {
@@ -25,25 +26,42 @@ function initList(cats) {
 
 function renderCat(cat) {
     // Create elements for display area
-    const name = document.createElement("h1");
-    const image = document.createElement("img");
-    const counter = document.createElement("h3");
+    let name = renderName(cat);
+    let image = renderImage(cat);
+    let counter = renderClicks(cat);
+    clearCat();
     const div = document.createElement("div");
-
-    // Set up content
-    name.textContent = cat.name;
-    image.setAttribute("src", cat.image);
-    image.addEventListener("click", () => {
-        increament(cat);
-    });
-
-    counter.innerHTML = `Clicks: <span>${cat.clicks}</span>`;
-
-    display.innerHTML = "";
     div.append(name);
     div.append(image);
     div.append(counter);
     display.append(div);
+}
+
+function renderClicks(cat) {
+    const counter = document.createElement("h3");
+    counter.innerHTML = `Clicks: <span>${cat.clicks}</span>`;
+    return counter;
+}
+
+function renderImage(cat) {
+    const image = document.createElement("img");
+    image.setAttribute("src", cat.image);
+    image.classList.add("pointer");
+    image.addEventListener("click", () => {
+        increament(cat);
+    });
+    return image;
+}
+
+function renderName(cat) {
+    const name = document.createElement("h1");
+    name.classList.add("text-center");
+    name.textContent = cat.name;
+    return name;
+}
+
+function clearCat() {
+    display.innerHTML = "";
 }
 
 //octopus
@@ -67,106 +85,3 @@ function increament(currentCat) {
         }
     }
 }
-
-// // add event listerner
-// document.querySelector("#andy").addEventListener("click", showAndy);
-// document.querySelector("#bob").addEventListener("click", showBob);
-// document.querySelector("#charlie").addEventListener("click", showCharlie);
-// document.querySelector("#david").addEventListener("click", showDavid);
-// document.querySelector("#effy").addEventListener("click", showEffy);
-
-// // select display area
-// const display = document.querySelector("#display");
-
-// // Create elements for display area
-// const name = document.createElement("h1");
-// const image = document.createElement("img");
-// const counter = document.createElement("h3");
-// const div = document.createElement("div");
-
-// //declare counter vaiable
-// let andyClick = 0;
-// let bobClick = 0;
-// let charlieClick = 0;
-// let davidClick = 0;
-// let effyClick = 0;
-
-// // add event listener to the image
-// image.addEventListener("click", increament);
-
-// function increament() {
-//     switch (name.textContent) {
-//         case "Andy":
-//             andyClick++;
-//             counter.innerHTML = `Clicks: <span>${andyClick}</span>`;
-//             break;
-//         case "Bob":
-//             bobClick++;
-//             counter.innerHTML = `Clicks: <span>${bobClick}</span>`;
-//             break;
-//         case "Charlie":
-//             charlieClick++;
-//             counter.innerHTML = `Clicks: <span>${charlieClick}</span>`;
-//             break;
-//         case "David":
-//             davidClick++;
-//             counter.innerHTML = `Clicks: <span>${davidClick}</span>`;
-//             break;
-//         case "Effy":
-//             effyClick++;
-//             counter.innerHTML = `Clicks: <span>${effyClick}</span>`;
-//             break;
-//     }
-// }
-
-// function showAndy() {
-//     name.textContent = "Andy";
-//     image.setAttribute("src", "andy.jpg");
-//     counter.innerHTML = `Clicks: <span>${andyClick}</span>`;
-
-//     div.append(name);
-//     div.append(image);
-//     div.append(counter);
-//     display.append(div);
-// }
-
-// function showBob() {
-//     name.textContent = "Bob";
-//     image.setAttribute("src", "bob.jpg");
-//     counter.innerHTML = `Clicks: <span>${bobClick}</span>`;
-
-//     div.append(name);
-//     div.append(image);
-//     div.append(counter);
-//     display.append(div);
-// }
-// function showCharlie() {
-//     name.textContent = "Charlie";
-//     image.setAttribute("src", "charlie.jpg");
-//     counter.innerHTML = `Clicks: <span>${charlieClick}</span>`;
-
-//     div.append(name);
-//     div.append(image);
-//     div.append(counter);
-//     display.append(div);
-// }
-// function showDavid() {
-//     name.textContent = "David";
-//     image.setAttribute("src", "david.jpg");
-//     counter.innerHTML = `Clicks: <span>${davidClick}</span>`;
-
-//     div.append(name);
-//     div.append(image);
-//     div.append(counter);
-//     display.append(div);
-// }
-// function showEffy() {
-//     name.textContent = "Effy";
-//     image.setAttribute("src", "effy.jpeg");
-//     counter.innerHTML = `Clicks: <span>${effyClick}</span>`;
-
-//     div.append(name);
-//     div.append(image);
-//     div.append(counter);
-//     display.append(div);
-// }
