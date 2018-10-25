507bf92c2c7ba8cd30c9c1541dcb286a43516720
Shiyu Wang <shiyu3169@gmail.com>
2018-10-25 14:01:22 -0400

    separate functions into different scope

--- main.js
+++ main.js
@@ -1,87 +1,98 @@
 // model
-var cats = [
-    { name: "Andy", image: "andy.jpg", clicks: 0 },
-    { name: "Bob", image: "bob.jpg", clicks: 0 },
-    { name: "Charlie", image: "charlie.jpg", clicks: 0 },
-    { name: "David", image: "david.jpg", clicks: 0 },
-    { name: "Effy", image: "effy.jpeg", clicks: 0 }
-];
+const model = {
+    cats: [
+        { name: "Andy", image: "andy.jpg", clicks: 0 },
+        { name: "Bob", image: "bob.jpg", clicks: 0 },
+        { name: "Charlie", image: "charlie.jpg", clicks: 0 },
+        { name: "David", image: "david.jpg", clicks: 0 },
+        { name: "Effy", image: "effy.jpeg", clicks: 0 }
+    ]
+};
 
 //view
-const catList = document.querySelector("#cats");
-const display = document.querySelector("#display");
+const view = {
+    catList: document.querySelector("#cats"),
+    display: document.querySelector("#display"),
 
-function initList(cats) {
-    for (cat of cats) {
+    renderListItem: function(cat) {
         const li = document.createElement("li");
         li.classList.add("pointer");
         li.classList.add("list-group-item");
         li.textContent = cat.name;
         li.addEventListener("click", e => {
-            renderCat(findCat(e.target.textContent));
+            this.renderCat(octopus.findCat(e.target.textContent));
         });
-        catList.append(li);
+        this.catList.append(li);
+    },
+
+    renderCat: function(cat) {
+        // Create elements for display area
+        let name = this.renderName(cat);
+        let image = this.renderImage(cat);
+        let counter = this.renderClicks(cat);
+        this.clearCat();
+        const div = document.createElement("div");
+        div.append(name);
+        div.append(image);
+        div.append(counter);
+        display.append(div);
+    },
+
+    renderClicks: function(cat) {
+        const counter = document.createElement("h3");
+        counter.innerHTML = `Clicks: <span>${cat.clicks}</span>`;
+        return counter;
+    },
+
+    renderImage: function(cat) {
+        const image = document.createElement("img");
+        image.setAttribute("src", cat.image);
+        image.classList.add("pointer");
+        image.addEventListener("click", () => {
+            octopus.increament(cat);
+        });
+        return image;
+    },
+
+    renderName: function(cat) {
+        const name = document.createElement("h1");
+        name.classList.add("text-center");
+        name.textContent = cat.name;
+        return name;
+    },
+
+    clearCat: function() {
+        display.innerHTML = "";
     }
-}
-
-function renderCat(cat) {
-    // Create elements for display area
-    let name = renderName(cat);
-    let image = renderImage(cat);
-    let counter = renderClicks(cat);
-    clearCat();
-    const div = document.createElement("div");
-    div.append(name);
-    div.append(image);
-    div.append(counter);
-    display.append(div);
-}
-
-function renderClicks(cat) {
-    const counter = document.createElement("h3");
-    counter.innerHTML = `Clicks: <span>${cat.clicks}</span>`;
-    return counter;
-}
-
-function renderImage(cat) {
-    const image = document.createElement("img");
-    image.setAttribute("src", cat.image);
-    image.classList.add("pointer");
-    image.addEventListener("click", () => {
-        increament(cat);
-    });
-    return image;
-}
-
-function renderName(cat) {
-    const name = document.createElement("h1");
-    name.classList.add("text-center");
-    name.textContent = cat.name;
-    return name;
-}
-
-function clearCat() {
-    display.innerHTML = "";
-}
+};
 
 //octopus
-initList(cats);
 
-function findCat(name) {
-    for (cat of cats) {
-        if (cat.name === name) {
-            return cat;
+const octopus = {
+    initList: function(cats) {
+        for (cat of cats) {
+            view.renderListItem(cat);
+        }
+    },
+
+    findCat: function(name) {
+        for (cat of model.cats) {
+            if (cat.name === name) {
+                return cat;
+            }
+        }
+        return null;
+    },
+
+    increament: function(currentCat) {
+        for (cat of model.cats) {
+            if (cat === currentCat) {
+                cat.clicks++;
+                view.renderCat(cat);
+                break;
+            }
         }
     }
-    return null;
-}
+};
 
-function increament(currentCat) {
-    for (cat of cats) {
-        if (cat === currentCat) {
-            cat.clicks++;
-            renderCat(cat);
-            break;
-        }
-    }
-}
+octopus.initList(model.cats);
